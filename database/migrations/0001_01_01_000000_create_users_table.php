2129cd3923a6f6d2b2b4ba0488f20291f5e65d5a
Timur Tsymzhitov <tstimurb0311@gmail.com>
2025-12-05 01:40:35 +0700

    fix: fix migration, add session table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,14 +23,29 @@ return new class extends Migration
             $table->string('email')
                 ->unique()
                 ->comment('Уникальный адрес электронной почты');
-            $table->timestamp('email_verified_at')
-                ->nullable();
             $table->string('phone', 20)
                 ->nullable()
                 ->unique()
                 ->comment('Номер телефона пользователя');
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
@@ -40,5 +55,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
