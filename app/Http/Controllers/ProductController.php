7cb1feabb0b1558d41591ea229cddfc6884e6ed0
Timur Tsymzhitov <tstimurb0311@gmail.com>
2025-12-16 11:50:49 +0700

    feat: create product page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\DTO\ProductFilterDto;
 use App\Http\Requests\ProductFilterRequest;
+use App\Models\Product;
 use App\Service\ProductService;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -20,6 +21,15 @@ class ProductController extends Controller
         return view('products.index', [
             'products' => $products,
             'dto'      => $dto,
+        ]);
+    }
+
+    public function show(Product $product, ProductService $service): Factory|View
+    {
+        $product = $service->getProduct($product);
+
+        return view('products.show', [
+            'product' => $product,
         ]);
     }
 }
